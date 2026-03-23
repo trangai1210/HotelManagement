8e09360d4e29170f400e9693c4c650bf66882cc3
TRÚC NGUYỄN NGỌC THANH <2354050152truc@ou.edu.vn>
2026-03-23 22:11:18 +0700

    commit

---
+++ HotelManagement/GUI/CustomerForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace HotelManagement.GUI
+{
+    partial class CustomerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelThongTin = new System.Windows.Forms.Panel();
+            this.txtTenKH = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbGioiTinh = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCccd = new System.Windows.Forms.TextBox();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.txtQuocTich = new System.Windows.Forms.TextBox();
+            this.panelThongTin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(220, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(472, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập thông tin khách hàng";
+            // 
+            // panelThongTin
+            // 
+            this.panelThongTin.BackColor = System.Drawing.Color.LightGray;
+            this.panelThongTin.Controls.Add(this.txtQuocTich);
+            this.panelThongTin.Controls.Add(this.txtDiaChi);
+            this.panelThongTin.Controls.Add(this.txtSDT);
+            this.panelThongTin.Controls.Add(this.txtCccd);
+            this.panelThongTin.Controls.Add(this.label3);
+            this.panelThongTin.Controls.Add(this.cbGioiTinh);
+            this.panelThongTin.Controls.Add(this.label2);
+            this.panelThongTin.Controls.Add(this.txtTenKH);
+            this.panelThongTin.Location = new System.Drawing.Point(253, 77);
+            this.panelThongTin.Name = "panelThongTin";
+            this.panelThongTin.Size = new System.Drawing.Size(395, 316);
+            this.panelThongTin.TabIndex = 1;
+            // 
+            // txtTenKH
+            // 
+            this.txtTenKH.BackColor = System.Drawing.Color.White;
+            this.txtTenKH.Location = new System.Drawing.Point(67, 14);
+            this.txtTenKH.Name = "txtTenKH";
+            this.txtTenKH.Size = new System.Drawing.Size(310, 26);
+            this.txtTenKH.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "👤";
+            // 
+            // cbGioiTinh
+            // 
+            this.cbGioiTinh.FormattingEnabled = true;
+            this.cbGioiTinh.Location = new System.Drawing.Point(67, 68);
+            this.cbGioiTinh.Name = "cbGioiTinh";
+            this.cbGioiTinh.Size = new System.Drawing.Size(310, 28);
+            this.cbGioiTinh.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "⚥";
+            // 
+            // txtCccd
+            // 
+            this.txtCccd.BackColor = System.Drawing.Color.White;
+            this.txtCccd.Location = new System.Drawing.Point(67, 124);
+            this.txtCccd.Name = "txtCccd";
+            this.txtCccd.Size = new System.Drawing.Size(310, 26);
+            this.txtCccd.TabIndex = 4;
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.BackColor = System.Drawing.Color.White;
+            this.txtSDT.Location = new System.Drawing.Point(67, 176);
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(310, 26);
+            this.txtSDT.TabIndex = 5;
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.BackColor = System.Drawing.Color.White;
+            this.txtDiaChi.Location = new System.Drawing.Point(67, 228);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(310, 26);
+            this.txtDiaChi.TabIndex = 6;
+            // 
+            // txtQuocTich
+            // 
+            this.txtQuocTich.BackColor = System.Drawing.Color.White;
+            this.txtQuocTich.Location = new System.Drawing.Point(67, 277);
+            this.txtQuocTich.Name = "txtQuocTich";
+            this.txtQuocTich.Size = new System.Drawing.Size(310, 26);
+            this.txtQuocTich.TabIndex = 7;
+            // 
+            // CustomerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 450);
+            this.Controls.Add(this.panelThongTin);
+            this.Controls.Add(this.label1);
+            this.Name = "CustomerForm";
+            this.Text = "CustomerForm";
+            this.panelThongTin.ResumeLayout(false);
+            this.panelThongTin.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelThongTin;
+        private System.Windows.Forms.TextBox txtTenKH;
+        private System.Windows.Forms.ComboBox cbGioiTinh;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtQuocTich;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.TextBox txtCccd;
+    }
+}
