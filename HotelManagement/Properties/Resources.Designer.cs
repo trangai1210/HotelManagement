3fc8cd494d5586c16ee54e99cf00c49e2b7b338d
Trangai <tn9594817@gmail.com>
2026-03-31 19:17:25 +0700

    sửa mainform chiếu hình

--- HotelManagement/Properties/Resources.Designer.cs
+++ HotelManagement/Properties/Resources.Designer.cs
@@ -63,9 +63,59 @@ namespace HotelManagement.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap hinhnhanvien {
+            get {
+                object obj = ResourceManager.GetObject("hinhnhanvien", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap hoboi {
+            get {
+                object obj = ResourceManager.GetObject("hoboi", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ksssss {
             get {
                 object obj = ResourceManager.GetObject("ksssss", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap phongngu {
+            get {
+                object obj = ResourceManager.GetObject("phongngu", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap quanlyks {
+            get {
+                object obj = ResourceManager.GetObject("quanlyks", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap sanh {
+            get {
+                object obj = ResourceManager.GetObject("sanh", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
