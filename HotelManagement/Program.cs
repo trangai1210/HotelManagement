079267cd62925d17a7cd0a6d579661b3d3450468
ivy2405 <2354050085nhi@ou.edu.vn>
2026-03-31 19:53:50 +0700

    hi

--- HotelManagement/Program.cs
+++ HotelManagement/Program.cs
@@ -17,7 +17,7 @@ namespace HotelManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ServiceForm());
+            Application.Run(new MainForm());
         }
     }
 }
